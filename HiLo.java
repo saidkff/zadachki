6cd7bcbf7b7b80b93f4fab3ff1bc51a4b0b91fdb
Zhavokhir Saidkulov Tursunkulovich <73753408+saidkff@users.noreply.github.com>
2022-05-24 16:13:30 +0600

    Add files via upload

--- HiLo.java
+++ HiLo.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int myNumber;
         String mess;
-
-        while (true){
-            int x = (int) (Math.random()*100+1);
-            while(true){
-                System.out.println("Gues a number between 1 and 100: ");
-                int myNumber = scan.nextInt();
-                if (myNumber<x){
-                    System.out.println(myNumber + " is too low. Try again! ");
-                }
-                else if(myNumber>x){
-                    System.out.println(myNumber + " is too high. Try again! ");
+        boolean flag = true;
+        int x = (int) (Math.random()*100+1);
+        while(flag){
+            System.out.println("Gues a number between 1 and 100: ");
+            myNumber = scan.nextInt();
+            if (myNumber>x){
+                System.out.println(myNumber + " is too high. Try again! ");
+            }
+            else if(myNumber<x){
+                System.out.println(myNumber + " is too lov. Try again! ");
+            }
+            else{
+                System.out.println(myNumber + " is correct! You Win!  ");
+                System.out.println("Would you like to play again? (y/n)");
+                mess = scan.next();
+                if (!mess.equals("y")){
+                    System.out.println("Good Luck!, Have a nice day! ");
+                    flag = false;
                 }
                 else{
-                    System.out.println(myNumber + " is correct! You Win!  ");
-                    break;
+                    x = (int) (Math.random()*100+1);
                 }
-            }
-            System.out.println("Would you like to play again? (y/n)");
-            mess = scan.next();
-            if (!mess.equals("y")){
-                System.out.println("GOOD LUCK!");
-                break;
             }
         }
     }
